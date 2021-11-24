6ce10adbf40271bcac038a4fde6a6d04da7eb059
casimiro_ciancimino <casimiro.ciancimino@tui.com>
2021-11-24 10:40:34 +0100

    feat: aggiunta della logica del carrello

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-function createProduct(parent, imgUrl, productTitle, textPrice) {
+function createProduct(parent, imgUrl, productTitle, textPrice, idProduct) {
   const product = document.createElement("div");
   product.className = "product";
+  product.setAttribute("id", idProduct);
 
   createImg(product, imgUrl, productTitle);
   createText(product, productTitle, textPrice);
   parent.appendChild(product);
+
+  product.addEventListener("click", (e) => {
+    cartList.push(
+      productsList.find(
+        (product) => parseInt(e.currentTarget.id) === product.id
+      )
+    );
+    alert(`Prodotto aggiunto al carrello, numero prodotti: ${cartList.length}`);
+  });
 }
 
 function createImg(parent, imgUrl, productTitle) {
@@ -25,18 +35,15 @@ function createText(parent, productTitle, textPrice) {
   parent.append(title, price);
 }
 
-// fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
-//   .then((response) => response.json())
-//   .then((data) => {
-//     products = data;
-//     renderProducts();
-//   });
-
-const wrapperProducts = document.querySelector(".wrapper__products");
-
 function renderProducts(listItems) {
   listItems.map((product) => {
-    createProduct(wrapperProducts, product.image, product.title, product.price);
+    createProduct(
+      wrapperProducts,
+      product.image,
+      product.title,
+      product.price,
+      product.id
+    );
   });
 }
 
@@ -44,8 +51,25 @@ function renderProducts(listItems) {
 const getProductsList = async () => {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
+  productsList = data;
+
+  // Nella eventualità di aggiungere una quantità per prodotto
+  // productsList = data.map((product) => {
+  //   product.quantity = 0;
+  //   return product;
+  // });
 
   return renderProducts(data);
 };
 
+const wrapperProducts = document.querySelector(".wrapper__products");
+const cartBtn = document.querySelector(".cartBtn");
+
+const cartList = [];
+let productsList = [];
+
 getProductsList();
+
+// cartBtn.addEventListener("click", () => {
+//   console.log(cartList);
+// });
